b92b02bc12a8d9f30e5e3dac816ab834fe1c3489
Kevin Anderson <kevinanderso@gmail.com>
2014-04-12 11:55:35 -0400

    Initial conditions done with text files, various UI changes.

--- SolarSystemDynamics.m
+++ SolarSystemDynamics.m
@@ -1,28 +1,7 @@
 function varargout = SolarSystemDynamics(varargin)
-    % SOLARSYSTEMDYNAMICS MATLAB code for SolarSystemDynamics.fig
-    %      SOLARSYSTEMDYNAMICS, by itself, creates a new SOLARSYSTEMDYNAMICS or raises the existing
-    %      singleton*.
-    %
-    %      H = SOLARSYSTEMDYNAMICS returns the handle to a new SOLARSYSTEMDYNAMICS or the handle to
-    %      the existing singleton*.
-    %
-    %      SOLARSYSTEMDYNAMICS('CALLBACK',hObject,~,handles,...) calls the local
-    %      function named CALLBACK in SOLARSYSTEMDYNAMICS.M with the given input arguments.
-    %
-    %      SOLARSYSTEMDYNAMICS('Property','Value',...) creates a new SOLARSYSTEMDYNAMICS or raises the
-    %      existing singleton*.  Starting from the left, property value pairs are
-    %      applied to the GUI before SolarSystemDynamics_OpeningFcn gets called.  An
-    %      unrecognized property name or invalid value makes property application
-    %      stop.  All inputs are passed to SolarSystemDynamics_OpeningFcn via varargin.
-    %
-    %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-    %      instance to run (singleton)".
-    %
-    % See also: GUIDE, GUIDATA, GUIHANDLES
-
     % Edit the above text to modify the response to help SolarSystemDynamics
 
-    % Last Modified by GUIDE v2.5 29-Mar-2014 16:43:54
+    % Last Modified by GUIDE v2.5 11-Apr-2014 19:13:59
 
     % Begin initialization code - DO NOT EDIT
     gui_Singleton = 1;
@@ -46,12 +25,6 @@ end
 
 % --- Executes just before SolarSystemDynamics is made visible.
 function SolarSystemDynamics_OpeningFcn(hObject, ~, handles, varargin)
-    % This function has no output args, see OutputFcn.
-    % hObject    handle to figure
-    % ~  reserved - to be defined in a future version of MATLAB
-    % handles    structure with handles and user data (see GUIDATA)
-    % varargin   command line arguments to SolarSystemDynamics (see VARARGIN)
-
     % Choose default command line output for SolarSystemDynamics
     handles.output = hObject;
     % Update handles structure
@@ -59,7 +32,16 @@ function SolarSystemDynamics_OpeningFcn(hObject, ~, handles, varargin)
     % UIWAIT makes SolarSystemDynamics wait for user response (see UIRESUME)
     % uiwait(handles.MainFigure);
     reset(handles);
-    menuConfiguration_Callback(hObject,0,handles);
+%    clearvars;
+    clc;
+%    clear all;
+    cla;
+    global bodies;
+    bodies = [];
+    menuConfiguration_Callback(0,0,handles);
+    menuLaunchDate_Callback(handles.menuLaunchDate, 0, handles)
+
+    
 end
 
 % --- Outputs from this function are returned to the command line.
@@ -71,14 +53,6 @@ function varargout = SolarSystemDynamics_OutputFcn(~, ~, handles)
 
     % Get default command line output from handles structure
     varargout{1} = handles.output;
-end
-
-
-% --- Executes on slider movement.
-function initial_velocity_Callback(hObject, ~, handles) %#ok<DEFNU>
-    value = get(hObject,'Value');
-    set(handles.txt_velocity,'String',value);
-    initial_angle_Callback(handles.initial_angle, 0, handles);
 end
 
 function drawBodies(handles)
@@ -102,7 +76,9 @@ function drawBodies(handles)
     for i=1:length(bodies)
         if bodies(i).joined, continue; end
         % using rectangle allows doing circle diameter in AU instead of px
-        rectangle('Position',[transpose(bodies(i).pos - bodies(i).Radius) 2*bodies(i).Radius 2*bodies(i).Radius],'Curvature',1);
+        corner = transpose(bodies(i).pos - bodies(i).Radius);
+        %disp(bodies(i).Name);
+        rectangle('Position',[corner(1:2) 2*bodies(i).Radius 2*bodies(i).Radius],'Curvature',1);
         plot(bodies(i).xHist, bodies(i).yHist, bodies(i).Color);
     end
     % used for centering on the currently tracked body:
@@ -116,13 +92,13 @@ function drawBodies(handles)
         dy = 0.02*(yax(2)-yax(1));
         for i=1:length(bodies)
             if bodies(i).joined, continue; end
-            if (strcmp(bodies(i).Name,'Sun') || strcmp(bodies(i).parent.Name,'Sun')) && planetLabeling
+%            if strcmp(bodies(i).Name,'Sun') && planetLabeling
                 % bodies(i) is a planet or the sun
                 text(bodies(i).pos(1)+dx, bodies(i).pos(2)+dy, bodies(i).Name);
-            elseif bodies(i).numberOfChildren == 0 && ~ strcmp(bodies(i).parent.Name,'Sun') && moonLabeling
+%            elseif bodies(i).numberOfChildren == 0 && ~ strcmp(bodies(i).parent.Name,'Sun') && moonLabeling
                 % bodies(i) is a moon
-                text(bodies(i).pos(1)+dx, bodies(i).pos(2)-dy*1.5*(bodies(i).childNumber), bodies(i).Name);
-            end
+%                text(bodies(i).pos(1)+dx, bodies(i).pos(2)-dy*1.5*(bodies(i).childNumber), bodies(i).Name);
+%            end
             
         end
     end
@@ -131,7 +107,6 @@ end
 
 function btnGo_Callback(hObject, ~, handles) %#ok<DEFNU>
     global bodies;
-    global spaceship;
 
     if strcmp(get(hObject, 'String'), 'Pause')
         disp('pause was hit');
@@ -142,29 +117,24 @@ function btnGo_Callback(hObject, ~, handles) %#ok<DEFNU>
 
     disp('started');
     start = str2double(get(hObject,'UserData'));
-    if start == 1 
-        v0 = get(handles.initial_velocity,'value');
-        t0 = get(handles.initial_angle,'value');
-        spaceship.vel = [v0*cos(t0); v0*sin(t0)];
-    end
+
     set(hObject, 'String', 'Pause');
-    set(handles.initial_velocity,'Enable','off');
-    set(handles.initial_angle, 'Enable', 'off');
     set(handles.menuConfiguration, 'Enable', 'off');
     set(handles.txtTimeStep, 'Enable','off');
     set(handles.txtFrameSkip, 'Enable','off');
     set(handles.btnReset, 'Enable','off');
     set(handles.menuMethod, 'Enable', 'off');
-    
+    set(handles.menuLaunchDate, 'Enable', 'off');
+
     deltaT = str2double(get(handles.txtTimeStep, 'String'));
     frameSkip = str2double(get(handles.txtFrameSkip, 'String'));
 
-    for i = bodies
-        fprintf(1,'Name: %s; x: %f; y: %f;\n', i.Name, i.pos(1), i.pos(2));
-    end
+%    for i = bodies
+%        fprintf(1,'Name: %s; x: %f; y: %f;\n', i.Name, i.pos(1), i.pos(2));
+%    end
 
-    % 10000 is the arbitrary number of iterations I picked
-    for t=start:70000
+    % 100000 is the arbitrary number of iterations I picked
+    for t=start:100000
         % window was closed?
         if ~ ishandle(handles.MainFigure)
             break;
@@ -213,8 +183,8 @@ function btnGo_Callback(hObject, ~, handles) %#ok<DEFNU>
             %first, calculate k1 for all particles, then k2 for all particles,
             %etc
             order = 4;
-            kr = zeros(2,order,length(bodies));
-            kv = zeros(2,order,length(bodies));
+            kr = zeros(3,order,length(bodies));
+            kv = zeros(3,order,length(bodies));
             %set up kr1 and kv1 for all bodies:
             for i = 1:length(bodies)
                 if bodies(i).joined, continue; end
@@ -286,8 +256,8 @@ function btnGo_Callback(hObject, ~, handles) %#ok<DEFNU>
             dV = zeros(2,length(bodies));
             for i = 1:length(bodies)
                 if bodies(i).joined, continue; end
-                accel = [0;0];
-                iAccel = [0;0];
+                accel = [0;0;0];
+                iAccel = [0;0;0];
                 for j = 1:length(bodies)
                     if bodies(j).joined, continue; end
                     if i ~= j
@@ -354,81 +324,9 @@ function accel = forceOn(body1, body2)
     G = 0.00011835;
     r = norm(body1.pos - body2.pos);
     % next line has (x1-x2)/mag(x1-x2) to represent rhat
-    force = -G*body1.Mass*body2.Mass/ r^3 * (body1.pos - body2.pos);
-    accel = force / body1.Mass;
+    accel = -G*body2.Mass/ r^3 * (body1.pos - body2.pos);
 end
     
-function initial_angle_Callback(hObject, ~, handles)
-    global spaceship;
-    value = get(hObject,'Value');
-    set(handles.txt_angle,'String',value*180/pi);
-    if ~ strcmp(get(handles.btnGo, 'String'), 'Go')
-        return;
-    end
-    tempr = 0:0.01:get(handles.initial_velocity, 'Value');
-    drawBodies(handles);
-    hold on;
-    plot(spaceship.pos(1)+tempr*cos(value),spaceship.pos(2)+tempr*sin(value));
-    %axis(defaultAxes);
-    hold off;
-end
-
-function bodies = DefineBodies(configuration)
-    global spaceship;
-    global sun;
-    % pName,pOrbitRadius,pOrbitSpeed,pMass,pRadius,pColor
-    % 10m = 6.685e-11 AU
-    sun = Body('Sun', 0, 0, 333000, 0.004649, 'y');
-        spaceship = sun.makeMoon('Ship',3.8,2,0.001,6.685e-11,'k');
-        mercury = sun.makeMoon('Mercury', 0.387,10.1,0.055,1.6308e-5,'r');
-        venus = sun.makeMoon('Venus', 0.723, 7.378, 0.8150, 4.0454e-5, 'g');
-        earth = sun.makeMoon('Earth', 1, 6.282, 1, 4.2564e-5, 'b');
-            earthMoon = earth.makeMoon('Mona', 0.00257, 0.21544, 0.012300, 1.161e-5,'k');
-        mars = sun.makeMoon('Mars', 1.524, 5.076, 0.107, 2.263e-5,'r');
-            phobos = mars.makeMoon('Phobos', 6.2675e-5, 0.4507, 1.78477e-8, 7.5e-8,'k');
-            deimos = mars.makeMoon('Deimos',1.5684E-004, 2.8490E-002, 2.4718E-008, 4.1000E-008,'k');
-        jupiter = sun.makeMoon('Jupiter', 5.203, 2.762, 317.8, 4.6239e-4, 'y');
-            io = jupiter.makeMoon('Io',2.8190E-003, 3.6540E+000, 1.5000E-002, 1.2175E-005,'k');
-            europa = jupiter.makeMoon('Europa',4.4847E-003, 2.8960E+000, 8.0000E-003, 1.0433E-005,'k');
-            ganymede = jupiter.makeMoon('Ganymede', 7.1552E-003, 2.2940E+000, 2.5000E-002, 1.7608E-005,'k');
-            callisto = jupiter.makeMoon('Callisto', 1.2585E-002, 1.7294E+000, 1.8000E-002, 1.6110E-005,'k');
-        saturn = sun.makeMoon('Saturn', 9.529, 2.02, 95.3, 3.8313e-4, 'y');
-            mimas = saturn.makeMoon('Mimas', 1.2366E-003, 3.0107E+000, 6.3000E-006, 1.3250E-006,'k');
-            enceladus = saturn.makeMoon('Enceladus', 1.5906E-003, 2.6622E+000, 1.8000E-005, 1.6850E-006,'k');
-            tethys = saturn.makeMoon('Tethys', 1.9694E-003, 2.3925E+000, 1.0300E-004, 3.5500E-006,'k');
-            dione = saturn.makeMoon('Dione', 2.5227E-003, 2.1141E+000, 3.2800E-004, 3.7530E-006,'k');
-            rhea = saturn.makeMoon('Rhea', 3.5235E-003, 1.7885E+000, 3.9000E-004, 5.1060E-006,'k');
-            titan = saturn.makeMoon('Titan', 8.1677E-003, 1.1748E+000, 2.2500E-002, 1.7219E-005,'k');
-            iapetus = saturn.makeMoon('Iapetus', 2.3803E-002, 6.8820E-001, 3.0234E-004, 4.9100E-006,'k');
-        uranus = sun.makeMoon('Uranus', 19.19, 1.43, 14.6, 1.6889e-4, 'g');
-            miranda = uranus.makeMoon('Miranda', 8.6492E-004, 0.223346, 1.1030E-005, 1.5760E-006,'k');
-            ariel = uranus.makeMoon('Ariel', 1.2769E-003, 1.8495E-001, 2.2600E-004, 3.8700E-006,'k');
-            umbriel = uranus.makeMoon('Umbriel', 1.7781E-003, 0.156614, 2.0000E-004, 3.9080E-006,'k');
-            tatania = uranus.makeMoon('Titania', 2.9139E-003, 1.2216E-001, 5.9080E-004, 5.2700E-006,'k');
-            oberon = uranus.makeMoon('Oberon', 3.9006E-003, 0.105748, 5.0460E-004, 5.0900E-006,'k');
-        neptune = sun.makeMoon('Neptune', 30.06, 1.14, 17.23, 1.6412e-4, 'b');
-            triton = neptune.makeMoon('Triton', 2.3714E-003, 0.147284, 3.5900E-003, 9.0470E-006,'k');
-        
-    bodies = [];
-    
-    if strcmp(configuration, 'Full Solar System')
-        bodies = [spaceship sun mercury venus earth mars jupiter saturn uranus neptune earthMoon phobos deimos io europa ganymede callisto mimas enceladus tethys dione rhea titan iapetus miranda ariel umbriel tatania oberon triton];    
-    elseif strcmp(configuration, 'Sun, Planets only')
-        bodies = [spaceship sun mercury venus earth mars jupiter saturn uranus neptune];    
-    elseif strcmp(configuration, 'Sun, Earth, Moon')
-        bodies = [spaceship sun earth earthMoon];
-    end
-    
-    % make the system's net momentum zero:
-    comVel = [0;0];
-    for b = bodies
-        if ~ strcmp(b.Name, 'Sun')
-            comVel = comVel + b.vel * b.Mass;
-        end
-    end
-    sun.vel = -1 * comVel / sun.Mass;
-end
-
 function txtTimeStep_Callback(hObject, ~, ~) %#ok<DEFNU>
     val = get(hObject, 'String');
     if isnan(str2double(val))
@@ -457,38 +355,30 @@ end
 
 function reset(handles)
     global defaultAxes;
-    global bodies;
 
     defaultAxes = [-30 30 -30 30];
     axis(defaultAxes);
     set(handles.txtTimeStep, 'String', 0.00273);
     set(handles.txtFrameSkip, 'String', 10);
-    set(handles.initial_angle, 'Value', pi/2);
-    set(handles.initial_velocity,'Value',1);
     set(handles.btnGo, 'UserData', '1');
-    set(handles.txt_velocity,'String',get(handles.initial_velocity,'Value'));
-    set(handles.txt_angle,'String',180/pi*get(handles.initial_angle,'Value'));
     set(handles.FrameCount,'String',0);
     
-    set(handles.initial_velocity,'Enable','on');
-    set(handles.initial_angle, 'Enable', 'on');
     set(handles.menuConfiguration, 'Enable', 'on');
     set(handles.txtTimeStep, 'Enable','on');
     set(handles.txtFrameSkip, 'Enable','on');
     set(handles.menuMethod, 'Enable', 'on');
+    set(handles.menuLaunchDate, 'Enable', 'on');
     set(handles.btnGo, 'String', 'Go');
     
-    contents = cellstr(get(handles.menuConfiguration,'String'));
-    config = contents{get(handles.menuConfiguration,'Value')};
-    bodies = DefineBodies(config);
-    drawBodies(handles);
+    %contents = cellstr(get(handles.menuConfiguration,'String'));
+    %config = contents{get(handles.menuConfiguration,'Value')};
 end
 
 function btnReset_Callback(~, ~, handles) %#ok<DEFNU>
     reset(handles);
 end
 
-function menuMethod_Callback(~, ~, handles) %#ok<DEFNU>
+function menuMethod_Callback(~, ~, ~) %#ok<DEFNU>
     % maybe set a good default timestep here?
 end
 
@@ -505,18 +395,47 @@ function chkLabeling_Callback(~, ~, handles) %#ok<DEFNU>
     end
 end
 
-function menuConfiguration_Callback(~, ~, handles) %#ok<DEFNU>
-    global bodies;
+function menuConfiguration_Callback(~, ~, handles) 
+    global bodyData;
     contents = cellstr(get(handles.menuConfiguration,'String'));
-    config = contents{get(handles.menuConfiguration,'Value')};
-    bodies = DefineBodies(config);
-    nameList{1} = 'None';
-    for i = 1:length(bodies)
-        nameList{i+1} = bodies(i).Name;
+    configuration = contents{get(handles.menuConfiguration,'Value')};
+    
+    %bodyPositions is a list of tables
+    primaryData = readtable('body_data/info.txt');
+    secondaryData = readtable('body_data/mooninfo_1.txt');
+    
+    % bodyInfo.txt expects data of the form of
+    % BodyName, BodyMass, BodyRadius
+    
+    if strcmp(configuration, 'Full Solar System')
+        bodyData = [primaryData; secondaryData];
+    elseif strcmp(configuration, 'Sun, Planets only')
+        bodyData = primaryData;
+    elseif strcmp(configuration, 'Sun, Earth, Moon')
+        bodyData = [primaryData; secondaryData];
+        criteria = strcmp(bodyData.Name,'Sun') | strcmp(bodyData.Name,'Earth') | strcmp(bodyData.Name,'Moon');
+        bodyData = bodyData(criteria,:);
     end
+    for row = 1:size(bodyData,1)
+        mass = bodyData.Mass(row); % e20 kg
+        radius = bodyData.Radius(row); % km
+        bodyData.Mass(row) = mass * 1.6744252e-5; %convert to earth masses
+        bodyData.Radius(row) = radius * 6.685e-9;   %convert to au
+    end
+    % now, bodyData has only the data for the bodies we want
+    nameList{1} = 'Ship';
+    for i = 1:length(bodyData.Name)
+        nameList{i+1} = bodyData.Name{i};
+    end
+    
     set(handles.centeredBody,'String',nameList);
     set(handles.centeredBody,'Value',1);
-    drawBodies(handles);   
+    
+    sunFile = readtable('body_data/Sun.txt');
+    
+    set(handles.menuLaunchDate,'Enable','on');
+    set(handles.menuLaunchDate,'String',sunFile.epoch);
+    set(handles.menuLaunchDate,'Value',1);
 end
 
 
@@ -531,4 +450,64 @@ function chkMoonLabeling_Callback(~, ~, handles) %#ok<DEFNU>
        strcmp(get(handles.btnGo,'String'), 'Resume')
         drawBodies(handles);
     end
+end
+
+
+% --- Executes on selection change in menuLaunchDate.
+function menuLaunchDate_Callback(hObject, ~, handles) %#ok<DEFNU>
+    global spaceship;
+    global bodies;
+    global bodyData;
+    bodies = [];
+    
+    contents = cellstr(get(hObject,'String'));
+    date = contents{get(hObject,'Value')};
+    for i = 1:size(bodyData,1)
+        name = bodyData{i,1};
+        name = name{1};
+        mass = bodyData{i,2};
+        rad =  bodyData{i,3};
+        color = 'k';
+        pos = readtable(strcat('body_data/',name,'.txt'));
+        
+        for j=1:length(pos.epoch)
+            if strcmp(pos.epoch{j},date)
+                x = [pos.x(j); pos.y(j); pos.z(j)];
+                v = [pos.vx(j); pos.vy(j); pos.vz(j)];
+                %convert to AU/year
+                v = v*365.242;                
+                % pName,pPos,pVel,pMass,pRadius,pColor
+                bodies = [bodies Body(name, x, v, mass, rad, color)];
+                break;
+            end
+        end
+        %pos contains all the position data for the launch date
+    end
+    %cassini is -82 in horizons
+    % -1.727517569091358E-01,  9.669822755631888E-01, -5.710328315819876E-04,
+    % -1.714880581115672E-02, -2.022517204531316E-03,  2.918539695245248E-05
+    cassini_pos = [-3.632738231908875E-02;  9.224957383614817E-01;  1.825469610465980E-02];
+    cassini_vel = [-1.653096673886727E-02; -2.596033878048688E-03;  8.679572660558595E-05];
+    %cassini_pos = [-3.633e-2; 9.225e-1; 1.8255e-2];
+    %cassini_vel = [-1.653e-2; -2.596e-3; 8.6796e-5];
+    cassini_vel = cassini_vel * 365.242;  
+    spaceship = Body('Ship',cassini_pos,cassini_vel,0.000001,0.001,'b');
+    bodies = [bodies spaceship];
+    
+    comVel = [0;0;0];
+    for b = bodies
+        if ~ strcmp(b.Name, 'Sun')
+            comVel = comVel + b.vel * b.Mass;
+        end
+    end
+    for b = bodies
+       if strcmp(b.Name, 'Sun')
+           b.vel = -1 * comVel / b.Mass;
+       end
+    end
+    drawBodies(handles);
+
+end
+
+function menuLaunchDate_CreateFcn(hObject, eventdata, handles)
 end
